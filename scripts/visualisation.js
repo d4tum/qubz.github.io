8f8738db764d71c37825266e1e4d8717ea534ddf
Matthew Gregory Browne <odbgza@gmail.com>
2013-05-01 16:42:15 +1000

    Test for ajax'ing all JSON

--- scripts/visualisation.js
+++ scripts/visualisation.js
@@ -170,391 +170,402 @@ var d3LoadedCallback = function() {
 		$.ajax({
 			type: 'GET',
 			dataType: 'json',
-			url: 'http://staging.yourview.org.au/visualization/points.json?forum=1',
-			success: function(data) {
-				userdata = data;
-			retrievePoints(userdata);
+			url: 'http://staging.yourview.org.au/visualization/user_data.json?forum=1',
+			success: function(json) {
+				userdata = json;
+				retrievePoints(userdata);
 
-			function retrievePoints(userdata) {
-			d3.json("json/points.json", function(json) {
-				points = json;
-				initVisiblityArray();
-				findRange(points);
-				data = createData();
-				plotData();
-			});
-		}
+				function retrievePoints(userdata) {
+					$.ajax({
+						type: 'GET',
+						dataType: 'json',
+						url: 'http://staging.yourview.org.au/visualization/points.json?forum=1',
+						success: function(json) {
 
-		function initVisiblityArray() {
-			for (var i = 0; i < points.length; i++) {
-				visibility.push({
-					username: userdata[i].username,
-					enabled: true
-				});
-			}
-			console.log(visibility);
-		}
+							//d3.json("json/points.json", function(json) {
+							points = json;
+							initVisiblityArray();
+							findRange(points);
+							data = createData();
+							plotData();
 
-		Array.max = function(array) {
-			return Math.max.apply(Math, array);
-		};
+						}
 
-		Array.min = function(array) {
-			return Math.min.apply(Math, array);
-		};
+						//d3.json("json/points.json", function(json) {
 
-		function findRange(points) {
-			var xs = [];
-			var ys = [];
-
-			for (var i = 0; i < points.length; i++) {
-				xs.push(points[i][0]);
-				ys.push(points[i][1]);
-			}
-
-			var xRange = Array.max(xs) - Array.min(xs);
-			var yRange = Array.max(ys) - Array.min(ys);
-
-			if (xRange > yRange) range = Math.ceil(xRange) + 2;
-			else range = Math.ceil(yRange) + 2;
-		}
-
-		function createData() {
-			var dataset = [];
-			// Add the user data to the points
-			for (var i = 0; i < points.length; i++) {
-				if (visibility[i].enabled) {
-					dataset.push({
-						x: points[i][0],
-						y: points[i][1],
-						colour: userdata[i].colour,
-						cred: userdata[i].cred,
-						id: userdata[i].id,
-						link: userdata[i].link,
-						primary: userdata[i].primary,
-						username: userdata[i].username
+						//});
 					});
-				}
-			}
 
-			return dataset;
-		}
+					function initVisiblityArray() {
+						for (var i = 0; i < points.length; i++) {
+							visibility.push({
+								username: userdata[i].username,
+								enabled: true
+							});
+						}
+						console.log(visibility);
+					}
 
-		function scale(dataset) {
-			for (var i = 0; i < dataset.length; i++) {
-				dataset[i].x = (dataset[i].x + (range / 2)) * 50;
-				dataset[i].y = (dataset[i].y + (range / 2)) * 50;
-				// points[i][j] = (points[i][j] + 5) * 50;
-			}
-			return dataset
-		}
+					Array.max = function(array) {
+						return Math.max.apply(Math, array);
+					};
 
-		var previousIndex;
+					Array.min = function(array) {
+						return Math.min.apply(Math, array);
+					};
 
-		function chooseRandDummyFile() {
-			var array = [];
+					function findRange(points) {
+						var xs = [];
+						var ys = [];
 
-			path1 = "json/dummy_points1.json";
-			path2 = "json/dummy_points2.json";
-			path3 = "json/dummy_points3.json";
-			path4 = "json/dummy_points4.json";
-			path5 = "json/dummy_points5.json";
+						for (var i = 0; i < points.length; i++) {
+							xs.push(points[i][0]);
+							ys.push(points[i][1]);
+						}
 
-			array.push(path1);
-			array.push(path2);
-			array.push(path3);
-			array.push(path4);
-			array.push(path5);
+						var xRange = Array.max(xs) - Array.min(xs);
+						var yRange = Array.max(ys) - Array.min(ys);
 
-			// Make sure we choose an index different to the last one.
-			while (true) {
-				index = Math.floor((Math.random() * 5) + 1);
-				if (previousIndex != index) break;
-			}
+						if (xRange > yRange) range = Math.ceil(xRange) + 2;
+						else range = Math.ceil(yRange) + 2;
+					}
 
-			previousIndex = index;
-			// Adjust index for zero base
-			index -= 1;
+					function createData() {
+						var dataset = [];
+						// Add the user data to the points
+						for (var i = 0; i < points.length; i++) {
+							if (visibility[i].enabled) {
+								dataset.push({
+									x: points[i][0],
+									y: points[i][1],
+									colour: userdata[i].colour,
+									cred: userdata[i].cred,
+									id: userdata[i].id,
+									link: userdata[i].link,
+									primary: userdata[i].primary,
+									username: userdata[i].username
+								});
+							}
+						}
 
-			return array[index];
-		}
+						return dataset;
+					}
 
-		function plotData() {
-			var g = svg.selectAll("g")
-				.data(scale(data)).enter()
-				.append("g")
-				.on("mouseover", function(d) {
-				var sel = d3.select(this);
-				sel.moveToFront();
-				console.log(d.username);
-			})
+					function scale(dataset) {
+						for (var i = 0; i < dataset.length; i++) {
+							dataset[i].x = (dataset[i].x + (range / 2)) * 50;
+							dataset[i].y = (dataset[i].y + (range / 2)) * 50;
+							// points[i][j] = (points[i][j] + 5) * 50;
+						}
+						return dataset
+					}
 
-			var circle = g.append("circle")
-				.attr("cx", function(d) {
-				return d.x;
-			})
-				.attr("cy", function(d) {
-				return d.y;
-			})
-				.style("fill", function(d) {
-				return d.colour;
-			})
-				.transition()
-				.duration(700)
-				.attr("r", function(d) {
-				return 15;
-			});
+					var previousIndex;
 
-			g.append("svg:title").text(function(d) {
-				return d.username;
-			});
-		}
+					function chooseRandDummyFile() {
+						var array = [];
 
-		function updatePlot() {
-			d3.json(chooseRandDummyFile(), function(json) {
-				points = json;
-				findRange(points);
-				data = createData();
+						path1 = "json/dummy_points1.json";
+						path2 = "json/dummy_points2.json";
+						path3 = "json/dummy_points3.json";
+						path4 = "json/dummy_points4.json";
+						path5 = "json/dummy_points5.json";
 
-				// enter() and append() are omitted for a transision()
-				svg.selectAll("circle")
-					.data(scale(data))
-					.transition()
-					.duration(1100)
-					.attr("cx", function(d) {
-					return d.x;
-				})
-					.attr("cy", function(d) {
-					return d.y;
-				})
-					.attr("r", function(d) {
-					return 15;
-				})
-					.style("fill", function(d) {
-					return d3.rgb(d.colour);
+						array.push(path1);
+						array.push(path2);
+						array.push(path3);
+						array.push(path4);
+						array.push(path5);
+
+						// Make sure we choose an index different to the last one.
+						while (true) {
+							index = Math.floor((Math.random() * 5) + 1);
+							if (previousIndex != index) break;
+						}
+
+						previousIndex = index;
+						// Adjust index for zero base
+						index -= 1;
+
+						return array[index];
+					}
+
+					function plotData() {
+						var g = svg.selectAll("g")
+							.data(scale(data)).enter()
+							.append("g")
+							.on("mouseover", function(d) {
+							var sel = d3.select(this);
+							sel.moveToFront();
+							console.log(d.username);
+						})
+
+						var circle = g.append("circle")
+							.attr("cx", function(d) {
+							return d.x;
+						})
+							.attr("cy", function(d) {
+							return d.y;
+						})
+							.style("fill", function(d) {
+							return d.colour;
+						})
+							.transition()
+							.duration(700)
+							.attr("r", function(d) {
+							return 15;
+						});
+
+						g.append("svg:title").text(function(d) {
+							return d.username;
+						});
+					}
+
+					function updatePlot() {
+						d3.json(chooseRandDummyFile(), function(json) {
+							points = json;
+							findRange(points);
+							data = createData();
+
+							// enter() and append() are omitted for a transision()
+							svg.selectAll("circle")
+								.data(scale(data))
+								.transition()
+								.duration(1100)
+								.attr("cx", function(d) {
+								return d.x;
+							})
+								.attr("cy", function(d) {
+								return d.y;
+							})
+								.attr("r", function(d) {
+								return 15;
+							})
+								.style("fill", function(d) {
+								return d3.rgb(d.colour);
+							});
+						});
+					}
+
+					function toggleEntity(enable) {
+
+						data = createData();
+
+						if (!enable) {
+							svg.selectAll('circle')
+								.data(scale(data), function(d) {
+								return (d.username);
+							})
+								.exit().transition()
+								.attr("r", 0)
+								.remove();
+						} else {
+							svg.selectAll("circle")
+								.data(scale(data), function(d) {
+								return (d.username);
+							})
+								.enter()
+								.append("circle")
+								.on("mouseover", function(d) {
+								var sel = d3.select(this);
+								sel.moveToFront();
+								console.log(d.username);
+							})
+								.attr("cx", function(d) {
+								return d.x;
+							})
+								.attr("cy", function(d) {
+								return d.y;
+							})
+								.style("fill", function(d) {
+								return d.colour;
+							})
+								.transition()
+								.duration(700)
+								.attr("r", function(d) {
+								return 15;
+							});
+						}
+
+
+						console.log(data);
+
+					}
+
+					d3.selection.prototype.moveToFront = function() {
+						return this.each(function() {
+							this.parentNode.appendChild(this);
+						});
+					};
+
+
+					// ~~~~~~~~~~~~~~~~~~~~~~~~~~jQuery stuff~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+					// Create tabs
+					$(function() {
+						$("#tabs").tabs();
+					});
+
+					// Create sliders for the Areas tab with on stop callback to update the plot
+					$(function() {
+						$("#slider1").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider2").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider3").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider4").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider5").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider6").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider7").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider8").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider9").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider10").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider11").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider12").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+						$("#slider13").slider().slider("option", "min", -1).slider({
+							max: 1
+						}).on("slidestop", function(event, ui) {
+							updatePlot();
+						});
+					});
+
+					// 1. find the username of the circle clicked
+					// 2. toggle it's enabled state
+					// 3. create a new data array bad bind it to circle
+					// 4. call exit().remove()
+					// see - http://mbostock.github.io/d3/tutorial/circle.html
+					$(function() {
+						$("#button1").click(function() {
+							for (var i = 0; i < visibility.length; i++) {
+								if (visibility[i].username == 'Liberals') {
+									console.log(visibility[i].enabled);
+									visibility[i].enabled = !visibility[i].enabled;
+									toggleEntity(visibility[i].enabled);
+								}
+							}
+
+						});
+					});
+
+					$(function() {
+						$("#button2").click(function() {
+
+							for (var i = 0; i < visibility.length; i++) {
+								if (visibility[i].username == 'Labor') {
+									console.log(visibility[i].enabled);
+									visibility[i].enabled = !visibility[i].enabled;
+									console.log(visibility[i].enabled);
+									toggleEntity(visibility[i].enabled);
+								}
+							}
+
+						});
+					});
+
+					$(function() {
+						$("#button3").click(function() {
+
+							for (var i = 0; i < visibility.length; i++) {
+								if (visibility[i].username == 'Greens') {
+									console.log(visibility[i].enabled);
+									visibility[i].enabled = !visibility[i].enabled;
+									toggleEntity(visibility[i].enabled);
+								}
+							}
+
+						});
+					});
+
+					$(function() {
+						$("#button4").click(function() {
+
+							for (var i = 0; i < visibility.length; i++) {
+								if (visibility[i].username == 'Nationals') {
+									console.log(visibility[i].enabled);
+									visibility[i].enabled = !visibility[i].enabled;
+									toggleEntity(visibility[i].enabled);
+								}
+							}
+
+						});
+					});
+
+					$(function() {
+						$("#button5").click(function() {
+
+							for (var i = 0; i < visibility.length; i++) {
+								if (visibility[i].username == 'timvangelder') {
+									console.log(visibility[i].enabled);
+									visibility[i].enabled = !visibility[i].enabled;
+									toggleEntity(visibility[i].enabled);
+								}
+							}
+
+						});
+					});
+
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						console.log(textStatus, errorThrown);
+					}
 				});
-			});
-		}
 
-		function toggleEntity(enable) {
-
-			data = createData();
-
-			if (!enable) {
-				svg.selectAll('circle')
-					.data(scale(data), function(d) {
-					return (d.username);
-				})
-					.exit().transition()
-					.attr("r", 0)
-					.remove();
-			} else {
-				svg.selectAll("circle")
-					.data(scale(data), function(d) {
-					return (d.username);
-				})
-					.enter()
-					.append("circle")
-					.on("mouseover", function(d) {
-					var sel = d3.select(this);
-					sel.moveToFront();
-					console.log(d.username);
-				})
-					.attr("cx", function(d) {
-					return d.x;
-				})
-					.attr("cy", function(d) {
-					return d.y;
-				})
-					.style("fill", function(d) {
-					return d.colour;
-				})
-					.transition()
-					.duration(700)
-					.attr("r", function(d) {
-					return 15;
-				});
-			}
+			// Retrieve user data from userdata.json
+			//d3.json("json/user_data.json", function(json) {
+			//	userdata = json;
+			//	retrievePoints(userdata);
+			//});
 
 
-			console.log(data);
 
-		}
-
-		d3.selection.prototype.moveToFront = function() {
-			return this.each(function() {
-				this.parentNode.appendChild(this);
-			});
-		};
-
-
-		// ~~~~~~~~~~~~~~~~~~~~~~~~~~jQuery stuff~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		// Create tabs
-		$(function() {
-			$("#tabs").tabs();
 		});
-
-		// Create sliders for the Areas tab with on stop callback to update the plot
-		$(function() {
-			$("#slider1").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider2").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider3").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider4").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider5").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider6").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider7").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider8").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider9").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider10").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider11").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider12").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-			$("#slider13").slider().slider("option", "min", -1).slider({
-				max: 1
-			}).on("slidestop", function(event, ui) {
-				updatePlot();
-			});
-		});
-
-		// 1. find the username of the circle clicked
-		// 2. toggle it's enabled state
-		// 3. create a new data array bad bind it to circle
-		// 4. call exit().remove()
-		// see - http://mbostock.github.io/d3/tutorial/circle.html
-		$(function() {
-			$("#button1").click(function() {
-				for (var i = 0; i < visibility.length; i++) {
-					if (visibility[i].username == 'Liberals') {
-						console.log(visibility[i].enabled);
-						visibility[i].enabled = !visibility[i].enabled;
-						toggleEntity(visibility[i].enabled);
-					}
-				}
-
-			});
-		});
-
-		$(function() {
-			$("#button2").click(function() {
-
-				for (var i = 0; i < visibility.length; i++) {
-					if (visibility[i].username == 'Labor') {
-						console.log(visibility[i].enabled);
-						visibility[i].enabled = !visibility[i].enabled;
-						console.log(visibility[i].enabled);
-						toggleEntity(visibility[i].enabled);
-					}
-				}
-
-			});
-		});
-
-		$(function() {
-			$("#button3").click(function() {
-
-				for (var i = 0; i < visibility.length; i++) {
-					if (visibility[i].username == 'Greens') {
-						console.log(visibility[i].enabled);
-						visibility[i].enabled = !visibility[i].enabled;
-						toggleEntity(visibility[i].enabled);
-					}
-				}
-
-			});
-		});
-
-		$(function() {
-			$("#button4").click(function() {
-
-				for (var i = 0; i < visibility.length; i++) {
-					if (visibility[i].username == 'Nationals') {
-						console.log(visibility[i].enabled);
-						visibility[i].enabled = !visibility[i].enabled;
-						toggleEntity(visibility[i].enabled);
-					}
-				}
-
-			});
-		});
-
-		$(function() {
-			$("#button5").click(function() {
-
-				for (var i = 0; i < visibility.length; i++) {
-					if (visibility[i].username == 'timvangelder') {
-						console.log(visibility[i].enabled);
-						visibility[i].enabled = !visibility[i].enabled;
-						toggleEntity(visibility[i].enabled);
-					}
-				}
-
-			});
-		});
-
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				console.log(textStatus, errorThrown);
-			}
-		});
-
-		// Retrieve user data from userdata.json
-		//d3.json("json/user_data.json", function(json) {
-		//	userdata = json;
-		//	retrievePoints(userdata);
-		//});
-
-		
-
-	});
-};
+	};
 
 
 
-loadScript(jQueryUrl, jQueryLoadedCallback);
+	loadScript(jQueryUrl, jQueryLoadedCallback);
